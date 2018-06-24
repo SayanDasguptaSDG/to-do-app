7a9fd06bda660fb07b01d496e54911322dc0700b
SayanDasguptaSDG <sayan.dg1995@gmail.com>
2018-06-24 13:20:17 +0530

    style.css is modified, controller is modified, todo.ejs is added that contains the todo list

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var app=express();
 app.set('view engine', 'ejs');
 
 //static files
-app.use('/assets', express.static('./public'));
+app.use(express.static('./public'));
 
 //fires controllers
 todoController(app);
