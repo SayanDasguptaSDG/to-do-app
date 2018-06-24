8fa9323db2aeb8530d19a4febeeecebb0a517a8c
SayanDasguptaSDG <sayan.dg1995@gmail.com>
2018-06-24 15:58:26 +0530

    installed mongoose, created database in cloud using mlab, added data in the database

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,4 +1,20 @@
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+//connect to the database
+mongoose.connect('mongodb://sayan:sayan123@ds117111.mlab.com:17111/to-do-list');
+
+//create a schema
+var todoSchema = new mongoose.Schema({
+  item: String
+});
+
+//create a model
+var Todo = mongoose.model('Todo', todoSchema);
+var itemOne = Todo({item: 'go to sleep'}).save(function(err){
+  if (err) throw err;
+  console.log('item saved');
+});
 
 var data=[{item: 'brush your teeth'}, {item: 'clean the house'}, {item: 'go to study'}];
 var urlencodedParser = bodyParser.urlencoded({extended: false});
