0e0f4beeb662602111d2baef574f531344b42faa
SayanDasguptaSDG <sayan.dg1995@gmail.com>
2018-06-24 15:19:45 +0530

    dynamically data addition and deleteion operations are implemented

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,15 +1,24 @@
+var bodyParser = require('body-parser');
+
+var data=[{item: 'brush your teeth'}, {item: 'clean the house'}, {item: 'go to study'}];
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
 module.exports = function(app) {
 
   app.get('/todo', function(req, res) {
-    res.render('todo');
+    res.render('todo', {todos: data});
   });
 
-  app.post('/todo', function(req, res) {
-
+  app.post('/todo', urlencodedParser, function(req, res) {
+    data.push(req.body);
+    res.json(data);
   });
 
-  app.delete('/todo', function(req, res) {
-
+  app.delete('/todo/:item', function(req, res) {
+    data=data.filter(function(todo) {
+      return todo.item.replace(/ /g, '-') !== req.params.item;
+    });
+    res.json(data);
   });
 
 }
