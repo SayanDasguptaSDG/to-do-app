b6187f29a203f4a18e7cdb48499efaca1cb95c58
SayanDasguptaSDG <sayan.dg1995@gmail.com>
2018-06-24 16:30:31 +0530

    minimalistic fully functional todo app, with dynamically adding and deleting records in mongo db

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -11,30 +11,34 @@ var todoSchema = new mongoose.Schema({
 
 //create a model
 var Todo = mongoose.model('Todo', todoSchema);
-var itemOne = Todo({item: 'go to sleep'}).save(function(err){
-  if (err) throw err;
-  console.log('item saved');
-});
 
-var data=[{item: 'brush your teeth'}, {item: 'clean the house'}, {item: 'go to study'}];
+//var data=[{item: 'brush your teeth'}, {item: 'clean the house'}, {item: 'go to study'}];
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 module.exports = function(app) {
 
   app.get('/todo', function(req, res) {
-    res.render('todo', {todos: data});
+    //get data from the mongo db and pass it to the view
+    Todo.find({}, function(err, data) {
+      if (err)  throw err;
+      res.render('todo', {todos: data});
+    });
   });
 
   app.post('/todo', urlencodedParser, function(req, res) {
-    data.push(req.body);
-    res.json(data);
+    //get data from the view and add it to mongo db
+    var newTodo = Todo(req.body).save(function(err, data) {
+      if (err)  throw err;
+      res.json(data);
+    });
   });
 
   app.delete('/todo/:item', function(req, res) {
-    data=data.filter(function(todo) {
-      return todo.item.replace(/ /g, '-') !== req.params.item;
+    //delete the item from mongo db
+    Todo.find({item: req.params.item.replace(/\-/g, " ")}).remove(function(err, data) {
+      if (err)  throw err;
+      res.json(data);
     });
-    res.json(data);
   });
 
 }
