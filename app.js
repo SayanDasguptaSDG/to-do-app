6c95110976207573313b8fcba2d2e86110890eec
SayanDasguptaSDG <sayan.dg1995@gmail.com>
2018-06-27 13:49:16 +0530

    changed port number to 3500

--- app.js
+++ app.js
@@ -13,5 +13,5 @@ app.use(express.static('./public'));
 todoController(app);
 
 //listen to port
-app.listen(3000);
-console.log('You are listening to port 3000');
+app.listen(3500);
+console.log('You are listening to port 3500');
